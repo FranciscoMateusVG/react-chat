4f25707d512fff6d8942c869e35fb0ec0812e158
FranciscoMateusVG <49985463+FranciscoMateusVG@users.noreply.github.com>
2022-09-09 21:36:15 -0300

    adicionei algumas funcionalidades como listening on type e smooth scroll to last message

--- client/src/components/ChatBody.js
+++ client/src/components/ChatBody.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-const ChatBody = ({ messages }) => {
+const ChatBody = ({ messages, lastMessageRef, typingStatus }) => {
   const navigate = useNavigate()
 
   const handleLeaveChat = () => {
@@ -37,9 +37,9 @@ const ChatBody = ({ messages }) => {
             </div>
           )
         )}
-
+        <div ref={lastMessageRef} />
         <div className='message__status'>
-          <p>Someone is typing...</p>
+          <p>{typingStatus}</p>
         </div>
       </div>
     </>
